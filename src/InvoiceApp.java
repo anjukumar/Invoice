e752688ac8c65c498610f7f91afb26315c18c83f
anjukumar <anjukumar.ak1@gmail.com>
2016-09-07 09:58:00 -0400

    Invoice App

---
+++ src/InvoiceApp.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+public class InvoiceApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+		double myPrices[] = new double[100];
+		//add prices to array. 
+		//Use a counter to keep track of how many items are in your array.
+		int counter = 0;//add prices to array. 
+		Scanner keyboard = new Scanner(System.in);
+		double number=0;
+		double total =0;
+		System.out.println("Enter tax rate");
+		
+		double tax = keyboard.nextDouble(); 
+		keyboard.nextLine();
+				
+		for(int x=0 ; x <100; x++)
+		{
+			
+			System.out.println("Enter price");
+			number =keyboard.nextDouble(); 
+			counter +=1;
+			if(number==0)
+				break;
+			else 
+				myPrices[x]=number;
+			total = total + number;
+			
+		}	
+			
+			for (int i=0;i <=counter; i++)
+				{
+				System.out.printf("The price is %.02f\n",myPrices[i]);
+				} 
+	
+				
+		double Final = total* (tax/100);
+	 System.out.println("The total "+total);
+	 System.out.println("The tax : "+Final);
+				 
+				keyboard.close();
+
+
+		}
+
+	}
+
+	
+
+	
