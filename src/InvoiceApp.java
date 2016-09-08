eea882b29741f479ffc7e2a40ad0d3b6c5df727a
anjukumar <anjukumar.ak1@gmail.com>
2016-09-08 15:56:08 -0400

    Adding try catch exceptions

--- src/InvoiceApp.java
+++ src/InvoiceApp.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class InvoiceApp {
 
@@ -13,41 +14,51 @@ public class InvoiceApp {
 		double number=0;
 		double total =0;
 		System.out.println("Enter tax rate");
-		
+
 		double tax = keyboard.nextDouble(); 
 		keyboard.nextLine();
-				
-		for(int x=0 ; x <100; x++)
+		try
 		{
-			
-			System.out.println("Enter price");
-			number =keyboard.nextDouble(); 
-			counter +=1;
-			if(number==0)
-				break;
-			else 
-				myPrices[x]=number;
-			total = total + number;
-			
-		}	
-			
-			for (int i=0;i <=counter; i++)
+			for(int x=0 ; x <100; x++)
+			{
+
+				System.out.println("Enter price");
+				number =keyboard.nextDouble(); 
+				counter +=1;
+
+				if(number==0)
+					break;
+				else 
+				{	
+					myPrices[x]=number;
+					total = total + number;
+				}
+
+			}
+				for (int i=0;i <counter; i++)
 				{
-				System.out.printf("The price is %.02f\n",myPrices[i]);
+					System.out.printf("The price is %.02f\n",myPrices[i]);
 				} 
-	
-				
-		double Final = total* (tax/100);
-	 System.out.println("The total "+total);
-	 System.out.println("The tax : "+Final);
-				 
-				keyboard.close();
 
 
+				double Final = total* (tax/100);
+				System.out.println("The total "+total);
+				System.out.println("The tax : "+Final);
+
+			
+		}
+		catch(InputMismatchException Ex)
+		{
+			System.out.println("Input has to be a Number");
+			//System.out.println(Ex.getMessage());
 		}
 
+		finally
+		{
+			keyboard.close();
+		}
 	}
 
-	
+}
 
-	
+
